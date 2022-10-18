8457a93de76564b55c992e5a503690cc6c9f959f
Nebiyeluel Yohannes <63227971+Nebyou1341@users.noreply.github.com>
2022-10-18 23:24:32 +0300

    Create Counting valleys.js

---
+++ Counting valleys.js
@@ -0,0 +1,20 @@
+function countingValleys(steps, path) {
+    
+ let counter = 0
+    let valleyCount = 0
+
+    for(let i=0;i<=steps;i++){
+        
+        if(path[i] == 'D')
+            counter++;
+        else
+            counter--;
+    
+        if(path[i] =='U' && counter == 0){
+            valleyCount++;
+        }
+    }
+
+    
+    return valleyCount;
+    }
